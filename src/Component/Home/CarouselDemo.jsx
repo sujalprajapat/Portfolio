b165c80fea8a69067c0c962d47742cd1fee9520a
sujalprajapat <sujalprajapat49@gmail.com>
2026-05-04 10:54:57 +0530

    projectdetail chnage

--- src/Component/Home/CarouselDemo.jsx
+++ src/Component/Home/CarouselDemo.jsx
@@ -1,32 +1,18 @@
 
 import { Carousel } from "../../ExternalComponent/Carousel";
 export function CarouselDemo() {
-  const slideData = [
+const slideData = [
     {
-      title: "Mystic Mountains",
-      button: "Explore Component",
-      src: "https://images.unsplash.com/photo-1494806812796-244fe51b774d?q=80&w=3534&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      link: "https://kalathiyainfotech.com/"
+        title: "Kalathiya infotech",
+        button: "Visit",
+        link: "https://kalathiyainfotech.com/"
     },
     {
-      title: "Urban Dreams",
-      button: "Explore Component",
-      src: "https://images.unsplash.com/photo-1518710843675-2540dd79065c?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      link: "https://getstan.app/"
+        title: "Vs infotech",
+        button: "Visit",
+        link: "https://vsinfotech.ca/"
     },
-    {
-      title: "Neon Nights",
-      button: "Explore Component",
-      src: "https://images.unsplash.com/photo-1590041794748-2d8eb73a571c?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      link: "https://yoyoai.ca/"
-    },
-    {
-      title: "Desert Whispers",
-      button: "Explore Component",
-      src: "https://images.unsplash.com/photo-1679420437432-80cfbf88986c?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      link: "https://kalathiyainfotech.com/"
-    },
-  ];
+];
   return (
     <div className="relative overflow-hidden w-full h-full py-20">
       <Carousel slides={slideData} />
